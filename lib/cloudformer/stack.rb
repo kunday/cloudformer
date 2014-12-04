6d3293245ab2728cd0d0e25e1079753b430ed536
Todd Pickell <tapickell@gmail.com>
2014-12-04 11:09:45 -0500

    alter to accept http urls

--- lib/cloudformer/stack.rb
+++ lib/cloudformer/stack.rb
@@ -20,7 +20,7 @@ class Stack
   end
 
   def apply(template_file, parameters, disable_rollback=false, capabilities=[], notify=[])
-    if ( template_file =~ /^https:\/\/.*(.json)$/ )
+    if ( template_file =~ /^http:\/\/.*(.json)$/ )
       template = template_file
     elsif ( template_file =~ /^https:\/\/s3\S+\.amazonaws\.com\/(.*)/ )
       template = template_file
